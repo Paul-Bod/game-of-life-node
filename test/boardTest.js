7ee6bf0b0c2b43a568eb823c0de71c61ab22969c
Paul Ladyman <paul.ladyman@bbc.co.uk>
2015-01-29 22:34:32 +0000

    Find next interation of a seed and run the game of life.

--- test/boardTest.js
+++ test/boardTest.js
@@ -1,24 +1,33 @@
 var board = require('../lib/board');
 
-exports.createBoard = {
-    fromSeed: function (test) {
+exports.nextIteration = {
+    fromBoard: function (test) {
         var seed = [
             [0, 1, 0, 0, 0],
             [0, 0, 1, 0, 0],
             [1, 1, 1, 0, 0],
             [0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0]
-        ]
-
-        var expected = [
-            [{alive: false, neighbours: 1}, {alive: true, neighbours: 1}, {alive: false, neighbours: 2}, {alive: false, neighbours: 1}, {alive: false, neighbours: 0}],
-            [{alive: false, neighbours: 3}, {alive: false, neighbours: 5}, {alive: true, neighbours: 3}, {alive: false, neighbours: 2}, {alive: false, neighbours: 0}],
-            [{alive: true, neighbours: 1}, {alive: true, neighbours: 3}, {alive: true, neighbours: 2}, {alive: false, neighbours: 2}, {alive: false, neighbours: 0}],
-            [{alive: false, neighbours: 2}, {alive: false, neighbours: 3}, {alive: false, neighbours: 2}, {alive: false, neighbours: 1}, {alive: false, neighbours: 0}],
-            [{alive: false, neighbours: 0}, {alive: false, neighbours: 0}, {alive: false, neighbours: 0}, {alive: false, neighbours: 0}, {alive: false, neighbours: 0}]
-        ]
+        ];
         
-        test.deepEqual(expected, board.createBoard(seed), ['Board should be correctly set up based on seed']);
+        var first = [
+            [0, 0, 0, 0, 0],
+            [1, 0, 1, 0, 0],
+            [0, 1, 1, 0, 0],
+            [0, 1, 0, 0, 0],
+            [0, 0, 0, 0, 0]
+        ];
+        
+        var second = [
+            [0, 0, 0, 0, 0],
+            [0, 0, 1, 0, 0],
+            [1, 0, 1, 0, 0],
+            [0, 1, 1, 0, 0],
+            [0, 0, 0, 0, 0]
+        ];
+                
+        test.deepEqual(first, board.nextIteration(seed), ['Should correctly derive first iteration from seed']);
+        test.deepEqual(second, board.nextIteration(first), ['Should correctly derive second iteration from first']);
         test.done();
     }
 }
