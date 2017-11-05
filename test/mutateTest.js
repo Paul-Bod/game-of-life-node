58ce15e72936445bdd85242a01a5bee3a5936912
Paul-Ladyman <ladyman.paul@gmail.com>
2017-11-05 18:03:52 +0000

    ensure mutations happen only adjacent to alive cells

--- test/mutateTest.js
+++ test/mutateTest.js
@@ -14,34 +14,32 @@ function emptySeed() {
 }
 
 function randomIndex(min, max) {
-      return Math.floor(Math.random() * (max - min) + min);
+    return Math.floor(Math.random() * (max - min) + min);
 }
 
 function singleCellSeed() {
     const seed = emptySeed();
-    const xIndex = randomIndex(0, seed.length);
-    const row = seed[xIndex];
-    const yIndex = randomIndex(0, row.length);
-
-    seed[xIndex][yIndex] = 1;
+    seed[5][5] = 1;
     return seed
-
 }
 
 function manyResults(seedGenerator) {
     let seeds = [];
-    for(var i = 0; i < 10; i++) {
+    for(var i = 0; i < 100; i++) {
         seeds.push(seedGenerator());
     }
-    seeds.map(mutate.mutate);
-    return seeds;
+    return seeds.map(mutate.mutate);
 }
 
-function exactlyOneLiveCell(results) {
+function trueForAll(results, predicate) {
     return results
-        .map(result => _.flattenDeep(result).filter(x => x === 1).length === 1)
+        .map(predicate)
         .filter(result => result === true)
         .length === results.length;
+}
+
+function numLiveCells(result, n) {
+    return _.flattenDeep(result).filter(x => x === 1).length === n;
 }
 
 /*
@@ -57,6 +55,7 @@ function liveCellsAreAdjacent(results) {
     const aliveIndex = _.indexOf(flattened, 1);
     const leftTrimmed =  _.drop(flattened, aliveIndex-4)
     const adjacents =  _.dropRight(leftTrimmed, leftTrimmed.length - 9);
+
     const sum = _.reduce(adjacents, function(sum, n) {
         return sum + n;
     }, 0)  
@@ -64,25 +63,40 @@ function liveCellsAreAdjacent(results) {
     return sum >= 2;
 }
 
+function liveCellsAreAdjacent2(result) {
+    return result[4][4] === 1 ||
+        result[4][5] === 1 ||
+        result[4][6]  === 1||
+        result[5][4]  === 1||
+        result[5][6]  === 1||
+        result[6][4] === 1 ||
+        result[6][5] === 1 ||
+        result[6][6] === 1;
+}
+
 function givesDifferentResults(results) {
     const reduced = results.map(_.flattenDeep);
     const uniques = _.uniqWith(reduced, _.isEqual);
-    return uniques.length === results.length;
+    return uniques.length > 1;
 }
 
 exports.randomMutation = {
     mutateEmptySeed: function (test) {
         const results = manyResults(emptySeed); 
 
-        test.equal(true, exactlyOneLiveCell(results), ['An empty seed should be mutated to contain one live cell']);
+        test.equal(true, trueForAll(results, (result) => numLiveCells(result, 1)), ['An empty seed should be mutated to contain one live cell']);
         test.equal(true, givesDifferentResults(results), ['An empty seed should produce different results for each mutation'])
         test.done();
     },
     mutateSimpleSeed: function (test) {
         const results = manyResults(singleCellSeed);
 
+        test.equal(true, trueForAll(results, (result) => numLiveCells(result, 2)), ['A mutation should add one additional live cell']);
+        test.equal(true, trueForAll(results, liveCellsAreAdjacent2), ['Mutations should form adjacent to already living cells'])
         test.equal(true, givesDifferentResults(results), ['A simple seed should produce different results for each mutation'])
-        test.equal(true, liveCellsAreAdjacent(results), ['Mutations should form adjacent to already living cells'])
         test.done();
     }
 };
+
+// TODO tests for mutate.expandCoordinates inc. live cells in corners and edges
+// TODO test for repeated mutations
